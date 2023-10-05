eb753e43d81066f76df1699773de1c69d892e110
Carlos Fernando Mamani <carlosfernando21334@gmail.com>
2023-10-05 12:37:41 -0300

    se termina la pagina de skills

--- src/app/model/proyecto.ts
+++ src/app/model/proyecto.ts
@@ -1,8 +1,12 @@
 export class Proyecto {
     public stack!:string[];
+    public title!:string; 
+    public desc!:string; 
+    public image!:string[]
 
     constructor()
     {
         this.stack = new Array(); 
+        this.image = new Array(); 
     }
 }
